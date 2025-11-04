c849cbfae54aa61a377146bcc7e4f07b8205b57e
Paula14P <paulaphilip31212@gmail.com>
2025-11-04 11:11:37 -1200

    Add files via upload

---
+++ gametracker-backend/server.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+const gamesRoutes = require('./src/routes/games');
+const reviewsRoutes = require('./src/routes/reviews');
+const errorHandler = require('./src/middlewares/errorHandler');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/juegos', gamesRoutes);
+app.use('/api/reseñas', reviewsRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 4000;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(()=> {
+    console.log('Mongo conectado');
+    app.listen(PORT, ()=> console.log(`Server run en puerto ${PORT}`));
+  })
+  .catch(err => console.error(err));
